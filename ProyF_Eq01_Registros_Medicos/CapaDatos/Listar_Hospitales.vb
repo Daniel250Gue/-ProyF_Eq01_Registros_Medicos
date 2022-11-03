4e340b5ea1b02b1a06f73d4a1a7682f1d2e4ab78
Daniel250Gue <84732717+Daniel250Gue@users.noreply.github.com>
2022-11-03 13:42:49 -0500

    ProyectoFinal
    DS8

---
+++ ProyF_Eq01_Registros_Medicos/CapaDatos/Listar_Hospitales.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Listar_Hospitales
+
+
+    'ASI CREAMOS METODOS PARA SER USADOS EN LA CAPA PRESENTACION A CADA METODO SE APERTURA LA BASE Y SE DEBE CERRAR AL FINAL
+    Public Shared Function NOMBREDELMETODO() As DataTable
+
+        Dim Conexion As New SqlConnection(My.Settings.cadena)
+        Conexion.Open()
+
+        Using Da As New SqlDataAdapter("NOMBRE DEL PROCEDIMIENTO", Conexion)
+
+            Using NOMBRE_VARIABLE As New DataTable
+                Da.Fill(NOMBRE_VARIABLE)
+
+
+                Return NOMBRE_VARIABLE
+
+            End Using
+        End Using
+        Conexion.Close()
+
+    End Function
+End Class
---
+++ ProyF_Eq01_Registros_Medicos/CapaPresentacion/Presentacion.vb
@@ -0,0 +1,11 @@
+﻿Public Class Presentacion
+    Private Sub Presentacion_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+        'ASI SE LLAMA LA CAPADATOS EL NOMBRE DE LA CLASE Y LOS METODOS DE ESAS CLASES
+
+        CapaDatos.Listar_Hospitales.NOMBREDELMETODO()
+
+
+    End Sub
+End Class
